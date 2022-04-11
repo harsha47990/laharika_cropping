a49768a7b183b19db30f68cac83b896e4fedaa7b
harsha47990 <harsha.martha@gmail.com>
2022-04-11 17:54:48 +0530

    r1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("laharika_2")]
+[assembly: AssemblyTitle("laharika cropping")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("laharika_2")]
+[assembly: AssemblyProduct("laharika cropping")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
